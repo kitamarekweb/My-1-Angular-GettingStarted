7b6a27f4765f021c1a279b88d7d0ec17f15eebfe
marek.kita <marek.kita@accenture.com>
2020-06-26 16:07:28 +0200

    create next FEATURE MODULE - EXPORT Component for other Modules

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,12 +1,10 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {ProductListComponent} from './product-list.component';
 import {ProductDetailComponent} from './product-detail.component';
 import {ConvertToSpacesPipe} from '../shared/convert-to-spaces.pipe';
-import {StarComponent} from '../shared/star.component';
-import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {ProductDetailGuard} from './product-detail.guard';
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
@@ -14,11 +12,8 @@ import {ProductDetailGuard} from './product-detail.guard';
     ProductListComponent,
     ProductDetailComponent,
     ConvertToSpacesPipe,
-    StarComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
       {
@@ -28,7 +23,8 @@ import {ProductDetailGuard} from './product-detail.guard';
         ],
         component: ProductDetailComponent
       },
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule {
